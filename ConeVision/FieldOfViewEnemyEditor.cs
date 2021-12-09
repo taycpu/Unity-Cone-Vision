fdc9ee71c9e5c04036b1aef492bd762dd0572942
taycpu <77157800+taycpu@users.noreply.github.com>
2021-12-09 13:52:45 +0300

    init

---
+++ ConeVision/FieldOfViewEnemyEditor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FieldOfViewEnemy))]
+public class FieldOfViewEnemyEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfViewEnemy enemy = (FieldOfViewEnemy) target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(enemy.transform.position, Vector3.up, Vector3.forward, 360, enemy.radius);
+
+        Vector3 viewAngle01 = DirectionFromAngle(enemy.transform.eulerAngles.y, -enemy.angle / 2);
+        Vector3 viewAngle02 = DirectionFromAngle(enemy.transform.eulerAngles.y, enemy.angle / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(enemy.transform.position, enemy.transform.position + viewAngle01 * enemy.radius);
+        Handles.DrawLine(enemy.transform.position, enemy.transform.position + viewAngle02 * enemy.radius);
+        if (enemy.canSeePlayer)
+        {
+            Handles.color = Color.green;
+        }
+    }
+
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
---
+++ ConeVision/IFOVReceiver.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public interface IFOVReceiver
+{
+    public void TargetEnterInArea(Transform target);
+    public void TargetInArea(Transform target);
+    public void TargetExitFromArea(Transform target);
+}
